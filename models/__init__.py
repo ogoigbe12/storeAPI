f3641f0f51b17be85ceda5892b784b043051590c
ogoigbe12 <ogoigbe12@gmail.com>
2024-08-29 12:57:57 +0100

    "Renamed `item_tags` to `item_tag`, removed `item_tags.py` file, updated relationships in `item.py`, `store.py`, and `tag.py` models, and updated imports in `__init__.py`."

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from models.item import ItemModel
 from models.store import StoreModel
 from models.tag import TagModel
-from models.item_tags import ItemsTags
+from models.item_tag import ItemsTags
 
 
